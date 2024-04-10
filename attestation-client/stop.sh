4b7c5893521dfe48030e849c0b05c55eeb64c56d
filipdf <filip@kravapolde.com>
2024-04-11 01:28:39 +0200

    update for new attestation client

--- attestation-client/stop.sh
+++ attestation-client/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z "$1" ]] ; then
-   docker compose -f docker-compose-attestation-client.yaml -p attestation-client down   
+   docker compose down
 else
-   docker compose -f docker-compose-attestation-client.yaml -p attestation-client stop $@
+   docker compose stop "$@"
 fi
