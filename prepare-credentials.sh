842a0e9e9bc749af429ce7ce3715364e490c3109
filipdf <filip@kravapolde.com>
2024-07-26 13:12:05 +0200

    use olympics-web2 attestation client docker image

--- prepare-credentials.sh
+++ prepare-credentials.sh
@@ -7,6 +7,6 @@ WORKING_DIR="$(pwd)"
 docker run -u root --rm \
     -v $WORKING_DIR/credentials:/app/attestation-suite-config \
     -v $WORKING_DIR/credentials.prepared:/app/credentials \
-    flarefoundation/attestation-client \
+    flarefoundation/attestation-client:olympics-web2 \
     yarn ts-node src/install/secureConfigurations.ts -o ../credentials
 
